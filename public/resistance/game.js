f9b6980e0a1c38f1d6424d4edd292042c35b95e3
Yuanwei Chen <redapple1900@gmail.com>
2016-01-30 15:18:41 -0800

    Increase Offline messages to 4.

---
+++ public/resistance/game.js
@@ -0,0 +1,111 @@
+﻿
+/**
+ * Module dependencies.
+ */
+var Emitter = require('events').EventEmitter;
+var player = require('./player.js');
+
+/**
+ * Module exports.
+ */
+
+module.exports = Game;
+
+/**
+ * Blacklisted events.
+ *
+ * @api public
+ */
+
+exports.events = [
+    'error',
+    'connect',
+    'disconnect',
+    'newListener',
+    'removeListener'
+];
+
+/**
+ * `EventEmitter#emit` reference.
+ */
+
+var emit = Emitter.prototype.emit;
+
+function Game(state) {
+    
+    var MAX_TURNS = 5
+    var MAX_TRIES = 5
+    var NUM_WINS = 3
+    var NUM_LOSSES = 3
+
+    this.state = state || State()
+    this.partcipants = [2, 3, 2, 3, 3]
+    
+    Game.prototype.run = function(){
+        
+    };
+    
+    Game.prototype.win = function () {
+        return this.state.wins >= NUM_WINS;
+    };
+    
+    Game.prototype.lose = function () {
+        return this.state.losses >= NUM_LOSSES;
+    };
+
+    Game.prototype.done = function () {
+        if (this.state.tries > MAX_TRIES) {
+            return true;
+        }
+        
+        if (this.state.turn > MAX_TURNS) {
+            return true;
+        }
+        
+        return this.win() || this.lose();
+    };
+    
+    Game.prototype.next_leader = function () {
+        var i
+        if (this.state.leader) {
+            i = ((this.state.leader.index + 1) % this.state.players.length)
+        } else {
+            i = 0
+        }
+
+        return this.state.players[i];
+    }
+
+    Game.prototype.performSelection = function () {
+        this.state.team = [];
+        this.state.votes = [];
+        this.state.sabotages = [];
+
+        // 
+        var count = this.partcipants[this.state.turn - 1];
+        
+        var selected = this.getSelection(count);
+
+    }
+    // TODO:: MORE functions
+}
+
+function State() {
+
+    var PHASE_PREPARING = 0
+    var PHASE_SELECTION = 1
+    var PHASE_VOTE = 2
+    var PHASE_MISSION = 3
+    var PHASE_ANNOUNING = 4
+
+    this.phase = 0
+    this.turn = 1
+    this.tries = 1
+    this.wins = 0
+    this.losses = 0
+    this.leader = null
+    this.team = null
+    this.players = [];
+    this.votes = [];
+    this.sabotages = null
+}
---
+++ public/resistance/player.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Module exports.
+ */
+
+module.exports = Player;
+
+
+function Player(game, name, index, identity) {
+    this.game = game;
+    this.name = name;
+    this.index = index;
+    this.identity = identity;
+}
